bd0ac2ccebf0aead2cd6b00599b6cd3dfcb44704
Supi <supimon@gmail.com>
2019-07-15 12:14:22 +0530

    moved the sticky nav script to demo.js

--- src/app.js
+++ src/app.js
@@ -8,23 +8,7 @@ $(document).ready(() => {
 
   "use strict";
 
-  $(".menu").click(function(){
-    $("#nav").toggleClass("open");
-  });
 
-  const $navbar = $(".nav"),
-    y_pos = $navbar.offset().top,
-    height = $navbar.height();
-
-  //scroll top 0 sticky
-  $(document).scroll(function() {
-    let scrollTop = $(this).scrollTop();
-    if (scrollTop > 0) {
-      $navbar.addClass("sticky");
-    } else {
-      $navbar.removeClass("sticky");
-    }
-  });
 
   console.log('Ready!');
 
--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -1,2 +1,19 @@
 console.log('Demo!');
 
+$(".menu").click(function(){
+  $("#nav").toggleClass("open");
+});
+
+const $navbar = $(".nav"),
+  y_pos = $navbar.offset().top,
+  height = $navbar.height();
+
+//scroll top 0 sticky
+$(document).scroll(function() {
+  let scrollTop = $(this).scrollTop();
+  if (scrollTop > 0) {
+    $navbar.addClass("sticky");
+  } else {
+    $navbar.removeClass("sticky");
+  }
+});
