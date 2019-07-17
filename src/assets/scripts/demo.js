659d583571a0dbeca0c5bc3158387b47fbfa402c
blessymoljames <blessymolj@gmail.com>
2019-07-17 17:30:20 +0530

    google search page sample design

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -1,19 +1,2 @@
 console.log('Demo!');
 
-$(".menu").click(function(){
-  $("#nav").toggleClass("open");
-});
-
-const $navbar = $(".nav"),
-  y_pos = $navbar.offset().top,
-  height = $navbar.height();
-
-//scroll top 0 sticky
-$(document).scroll(function() {
-  let scrollTop = $(this).scrollTop();
-  if (scrollTop > 0) {
-    $navbar.addClass("sticky");
-  } else {
-    $navbar.removeClass("sticky");
-  }
-});
