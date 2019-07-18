bef43d16288376315fbffa305846f2a43b3f64cf
Blessymol <blessymolj@gmail.com>
2019-07-18 23:35:37 +0530

    sticky nav and parallax effect using bs4

--- src/app.js
+++ src/app.js
@@ -10,9 +10,13 @@ import './assets/scripts/combine';
 import './scss/accordion.scss';
 import './assets/scripts/accordion';
 
+import './scss/combine-bs.scss';
+import './assets/scripts/combine-bs';
+
 $(document).ready(() => {
   console.log('Ready!');
 
   require('./assets/scripts/accordion');
   require('./assets/scripts/combine');
+  require('./assets/scripts/combine-bs');
 });
